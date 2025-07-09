c5d355ac796aa75b41e663147b0ce09bd3e94302
vinicius <vinicius.rodrigues.3@globahitss.com.br>
2025-07-09 17:24:47 -0300

    Create mapper

---
+++ src/infra/database/prisma/mappers/prisma-answer-attachmen-mapper.ts
@@ -0,0 +1,19 @@
+import { Attachment as PrismaAttachment } from '@prisma/client'
+import { AnswerAttachment } from '@/domain/forum/enterprise/entities/answer-attachment'
+import { UniqueEntityID } from '@/core/entities/unique-entity-i-d';
+
+export class PrismaAnswerAttachmentMapper {
+  static toDomain(raw: PrismaAttachment): AnswerAttachment {
+    if (!raw.answerId) {
+      throw new Error('Invalid attachment type.')
+    }
+
+    return AnswerAttachment.create(
+      {
+        attachmentId: new UniqueEntityID(raw.id),
+        answerId: new UniqueEntityID(raw.answerId),
+      },
+      new UniqueEntityID(raw.id),
+    )
+  }
+}
